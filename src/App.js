faa63325d05e408654ec33bd3ca0eed77d090df0
jstepcom <j.stepcom@gmail.com>
2021-09-04 22:20:10 -0500

    Create search engine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Dictionary from './Dictionary.js';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">   
+        <header className="App-header">
+         <h3>≈ Dictionary 📙≈</h3>
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <footer> Coded by JSCO  </footer>
+        <main>
+          <Dictionary />
+        </main>
+       
+      </div>
     </div>
   );
 }
