fe32c56c9c7151023de2a444555600fb544be001
jstepcom <j.stepcom@gmail.com>
2021-09-06 15:00:59 -0500

    style folder created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
-import Dictionary from './Dictionary.js';
+import './style/App.css';
 import Language from './Language';
 
 export default function App() {
-
-
 
   return (
     <div className="App">
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import "./Dictionary.css";
+import "./style/Dictionary.css";
 import Swal from 'sweetalert2'
 import axios from 'axios';
 
