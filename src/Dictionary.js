2f114256746275b2a73ada9d95b7d5154aefe05d
jstepcom <j.stepcom@gmail.com>
2021-09-05 22:59:53 -0500

    api function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,30 @@
 import React, {useState} from 'react';
 import "./Dictionary.css";
 import Swal from 'sweetalert2'
+import axios from 'axios';
 
 export default function Dictionary (){
   let [keyword, setKeyword] = useState("");
+  let [word,setWord] = useState("");
+  let [type, setType] = useState("");
+  let [meaning, setMeaning] = useState("");
+  
 
   function updateWord(event){
     setKeyword (event.target.value);
   }
 
+  function handleResponse(response){
+    word = response.data[0].word;
+    setWord(word = word.charAt(0).toUpperCase()+word.substring(1));
+    setType(response.data[0].meanings[0].partOfSpeech);
+    setMeaning(response.data[0].meanings[0].definitions[0].definition)
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+  }
+
   function search(event){
     event.preventDefault();
-    let msg = "";
-    if (keyword === ""){
-      msg = "Please type a world";
-    }else{
-      msg = `Searching for ${keyword}`;
-    }
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/`
     const Toast = Swal.mixin({
       toast: true,
       position: 'center-end',
@@ -28,15 +36,22 @@ export default function Dictionary (){
         toast.addEventListener('mouseleave', Swal.resumeTimer)
       }
     })
-    Toast.fire({
-      background:'#CEE5D0',
-      icon: 'success',
-      iconColor:'cadetblue',
-      title: msg,
-      confirmButtonText: 'Cool',
-    })
+    if (keyword === ""){
+      setWord("");
+      setType("");
+      setMeaning("");
+      Toast.fire({
+        background:'#CEE5D0',
+        icon: 'success',
+        iconColor:'cadetblue',
+        title: 'Please type a world',
+        confirmButtonText: 'Cool',
+      })
+    }else{
+      axios.get(apiUrl+keyword).then(handleResponse);
+    }
   }
-
+keyword = keyword.charAt(0).toUpperCase()+keyword.substring(1)
   return (
     <div className="Dictionary">
       <h5>Looking for:</h5>
@@ -48,6 +63,14 @@ export default function Dictionary (){
           <button className="D-btn" type="submit"> Search </button>
         </div>
       </form>  
+      <div className='D-def'>
+        <h3>{word}</h3>
+        <p>{type}</p>
+        <p>{meaning}</p>
+       
+
+      </div>
     </div>
+  
   );
 };
